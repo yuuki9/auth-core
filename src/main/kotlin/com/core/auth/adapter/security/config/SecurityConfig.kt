e20a153d0ad46605ecb500acc25e8890ccb840bc
jin ho-seong <101304194+yuuki9@users.noreply.github.com>
2025-07-06 23:46:59 +0900

    [UPDATE] security 패키지 위치 변경

---
+++ src/main/kotlin/com/core/auth/adapter/security/config/SecurityConfig.kt
@@ -0,0 +1,18 @@
+package com.core.auth.adapter.security.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+class SecurityConfig {
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
+        http.build()
+}
--- src/main/kotlin/com/core/auth/config/SecurityConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.core.auth.config
-
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-class SecurityConfig {
-}
